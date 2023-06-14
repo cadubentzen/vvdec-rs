64858dc267452e150e31d5fc50220862e61c95d3
Carlos Bentzen <cadubentzen@gmail.com>
2023-06-14 10:56:57 +0200

    build vvdec from source if not installed

--- vvdec-sys/build.rs
+++ vvdec-sys/build.rs
@@ -1,11 +1,68 @@
 use std::path::PathBuf;
 
+mod build {
+    use super::*;
+    use std::env;
+    use std::path::Path;
+    use std::process::{Command, Stdio};
+
+    const REPO: &str = "https://github.com/fraunhoferhhi/vvdec.git";
+
+    macro_rules! runner {
+        ($cmd:expr, $($arg:expr),*) => {
+            Command::new($cmd)
+                $(.arg($arg))*
+                .stderr(Stdio::inherit())
+                .stdout(Stdio::inherit())
+                .output()
+                .expect(concat!($cmd, " failed"));
+
+        };
+    }
+
+    pub fn build_from_src(
+        lib: &str,
+        version: &str,
+    ) -> Result<system_deps::Library, system_deps::BuildInternalClosureError> {
+        let mut tag = "v".to_string();
+        tag.push_str(version);
+
+        let source = PathBuf::from(env::var("OUT_DIR").unwrap()).join("vvdec");
+
+        if !Path::new(&source.join(".git")).exists() {
+            runner!("git", "clone", "--depth", "1", "-b", tag, REPO, &source);
+        } else {
+            runner!(
+                "git",
+                "-C",
+                source.to_str().unwrap(),
+                "fetch",
+                "--depth",
+                "1",
+                "origin",
+                tag
+            );
+            runner!(
+                "git",
+                "-C",
+                source.to_str().unwrap(),
+                "checkout",
+                "FETCH_HEAD"
+            );
+        }
+
+        let install_dir = cmake::build(source);
+        let pkg_config_dir = install_dir.join("lib/pkgconfig");
+        system_deps::Library::from_internal_pkg_config(&pkg_config_dir, lib, version)
+    }
+}
+
 fn main() {
     println!("cargo:rerun-if-changed=wrapper.h");
 
-    pkg_config::Config::new()
-        .atleast_version("2.0.0")
-        .probe("libvvdec")
+    system_deps::Config::new()
+        .add_build_internal("libvvdec", build::build_from_src)
+        .probe()
         .unwrap();
 
     let bindings = bindgen::Builder::default()

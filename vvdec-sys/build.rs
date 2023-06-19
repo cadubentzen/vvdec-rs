3ef092712c5c3681cca2a513e74c8b7d07aad6d0
Carlos Bentzen <cadubentzen@gmail.com>
2023-06-19 17:19:04 +0200

    add dev container and use that in CI (#1)

--- vvdec-sys/build.rs
+++ vvdec-sys/build.rs
@@ -1,8 +1,10 @@
+use std::env;
 use std::path::PathBuf;
+
+const VVDEC_VERSION: &str = "2.0.0";
 
 mod build {
     use super::*;
-    use std::env;
     use std::path::Path;
     use std::process::{Command, Stdio};
 
@@ -20,12 +22,9 @@ mod build {
         };
     }
 
-    pub fn build_from_src(
-        lib: &str,
-        version: &str,
-    ) -> Result<system_deps::Library, system_deps::BuildInternalClosureError> {
+    pub fn build_from_src() -> PathBuf {
         let mut tag = "v".to_string();
-        tag.push_str(version);
+        tag.push_str(VVDEC_VERSION);
 
         let source = PathBuf::from(env::var("OUT_DIR").unwrap()).join("vvdec");
 
@@ -53,20 +52,43 @@ mod build {
 
         let install_dir = cmake::build(source);
         let pkg_config_dir = install_dir.join("lib/pkgconfig");
-        system_deps::Library::from_internal_pkg_config(&pkg_config_dir, lib, version)
+        pkg_config_dir
     }
 }
 
 fn main() {
     println!("cargo:rerun-if-changed=wrapper.h");
 
-    system_deps::Config::new()
-        .add_build_internal("libvvdec", build::build_from_src)
-        .probe()
-        .unwrap();
+    // Try to find vvdec in the system. Else, build vvdec from source.
+    let library = match pkg_config::Config::new()
+        .atleast_version(VVDEC_VERSION)
+        .probe("libvvdec")
+    {
+        Ok(library) => library,
+        Err(_) => {
+            if env::var("VVDEC_SYS_BUILD_DEP_FROM_SRC").is_err() {
+                panic!(
+                    "libvvdec not found in the system. To allow building it from source, \
+                    set environment variable VVDEC_SYS_BUILD_DEP_FROM_SRC=1"
+                );
+            }
+            let pkg_config_dir = build::build_from_src();
+            env::set_var("PKG_CONFIG_PATH", pkg_config_dir.as_os_str());
+            pkg_config::Config::new()
+                .atleast_version(VVDEC_VERSION)
+                .probe("libvvdec")
+                .unwrap()
+        }
+    };
 
     let bindings = bindgen::Builder::default()
         .header("wrapper.h")
+        .clang_args(
+            library
+                .include_paths
+                .iter()
+                .map(|path| format!("-I{}", path.to_string_lossy())),
+        )
         .parse_callbacks(Box::new(bindgen::CargoCallbacks))
         .allowlist_type("vvdec.*")
         .allowlist_function("vvenc_.*")

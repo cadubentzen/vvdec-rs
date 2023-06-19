bcf41646699932876f964d66c54199a23d1c5985
Carlos Bentzen <cadubentzen@gmail.com>
2023-06-19 22:44:21 +0200

    add initial code to vvdec safe bindings

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,56 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use std::{mem::zeroed, ptr};
+use vvdec_sys::*;
+
+pub struct Params {
+    params: vvdecParams,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+impl Default for Params {
+    fn default() -> Self {
+        unsafe {
+            let mut params = zeroed::<vvdecParams>();
+            vvdec_params_default(&mut params);
+            Self { params }
+        }
+    }
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+// TODO
+pub enum Error {}
+
+// TODO
+pub struct Frame {}
+
+pub struct Decoder {
+    decoder: ptr::NonNull<vvdecDecoder>,
+}
+
+impl Decoder {
+    pub fn new() -> Option<Self> {
+        let default_params = Params::default();
+        Self::with_params(default_params)
+    }
+
+    pub fn with_params(mut params: Params) -> Option<Self> {
+        let decoder = unsafe { vvdec_decoder_open(&mut params.params) };
+        ptr::NonNull::new(decoder).map(|decoder| Self { decoder })
+    }
+
+    pub fn decode(
+        &mut self,
+        data: &[u8],
+        cts: Option<u64>,
+        dts: Option<u64>,
+        randomAccessPoint: bool,
+    ) -> Result<Option<Frame>, Error> {
+        todo!()
+    }
+}
+
+impl Drop for Decoder {
+    fn drop(&mut self) {
+        unsafe {
+            vvdec_decoder_close(self.decoder.as_ptr());
+        }
     }
 }

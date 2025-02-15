e5886996678dfe34548a16e698d5f077f18c618c
Carlos Bentzen <cadubentzen@igalia.com>
2025-02-15 15:56:59 +0100

    vvdec-sys: use system-deps
    By making use system-deps we can simplify the logic about whether to use
    libvvdec from the system or build a vendored version.

--- vvdec-sys/build.rs
+++ vvdec-sys/build.rs
@@ -2,41 +2,34 @@ use std::path::PathBuf;
 
 const VVDEC_VERSION: &str = "3.0.0";
 
-#[cfg(feature = "vendored")]
 mod vendored {
     use super::*;
     use std::str::FromStr;
 
-    pub fn build_from_src() {
+    pub fn build_from_src(
+        lib: &str,
+        _version: &str,
+    ) -> Result<system_deps::Library, system_deps::BuildInternalClosureError> {
+        println!("cargo:rerun-if-changed=vvdec");
         let source = PathBuf::from_str("vvdec").expect("submodule is initialized");
         let install_dir = cmake::Config::new(source)
             .define("VVDEC_TOPLEVEL_OUTPUT_DIRS", "OFF")
             .build();
-        let pkg_config_dir = install_dir.join("lib/pkgconfig");
-        std::env::set_var("PKG_CONFIG_PATH", pkg_config_dir.as_os_str());
-        println!("cargo:rerun-if-changed=vvdec");
+        let pkg_dir = install_dir.join("lib/pkgconfig");
+        system_deps::Library::from_internal_pkg_config(pkg_dir, lib, VVDEC_VERSION)
     }
 }
 
 fn main() {
     println!("cargo:rerun-if-changed=wrapper.h");
 
-    #[cfg(feature = "vendored")]
-    vendored::build_from_src();
-
-    let library = pkg_config::Config::new()
-        .atleast_version(VVDEC_VERSION)
-        .probe("libvvdec")
-        .expect("libvvdec not found in the system. To allow building it from source, use the \"vendored\" feature");
+    system_deps::Config::new()
+        .add_build_internal("libvvdec", vendored::build_from_src)
+        .probe()
+        .unwrap();
 
     let bindings = bindgen::Builder::default()
         .header("wrapper.h")
-        .clang_args(
-            library
-                .include_paths
-                .iter()
-                .map(|path| format!("-I{}", path.to_string_lossy())),
-        )
         .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()))
         .allowlist_type("vvdec.*")
         .allowlist_function("vvdec_.*")
